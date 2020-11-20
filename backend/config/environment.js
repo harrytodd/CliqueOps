91fd9d826c971b24475af5d5ae145edececb4551
Harry Todd <harrytodd1997@gmail.com>
2020-11-20 13:02:24 +0000

    clique

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -5,7 +5,7 @@ const env = process.env.NODE_ENV || 'development'
 
 const dbURI = env === 'production'
   ? process.env.MONGODB_URI
-  : `mongodb://localhost/pokedb-${env}`
+  : `mongodb://localhost/cliquedb-${env}`
 
 module.exports = {
   secret, port, dbURI
